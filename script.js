3fc34c6b18c9793c331cc27f92c514d1ad67ddf0
Alvaro <17alma41@gmail.com>
2023-11-28 14:48:22 +0100

    Practica-8

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function addWord(){
+    const text = document.querySelector("#text").value
+    let element = document.createElement("p");
+    console.log(element);
+    element.innerHTML = text
+    
+    const contenedor = document.querySelector("#div1");
+    contenedor.appendChild(element);
+}
